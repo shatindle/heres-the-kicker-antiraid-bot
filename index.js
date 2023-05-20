a8d889e1b97c63f04672af74a1719552394ea481
Shane Tindle <sha.tindle@gmail.com>
2023-05-20 16:00:52 -0500

    Revert "Logic based on user role count within a short period of time"
    This reverts commit c8f21dab9c53ad82297574f6383ef7d52ef2f4ae.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({ 
     intents: [
@@ -9,41 +9,58 @@ const client = new Client({
     ]
 });
 
-const REQUIRED_ROLE = process.env.REQUIRED_ROLE;
-const ROLE_CHECK_MS = parseInt(process.env.ROLE_CHECK_MINUTES) * 60 * 1000;
+let ids = {};
 
-const userList = {};
+const JOIN_COUNT = parseInt(process.env.JOIN_COUNT);
+const HOURS_LIMIT = 1000 * 60 * 60 * 12;
+const MONTHS_LIMIT = 1000 * 60 * 60 * 24 * 30 * 8;
 
 client.on("guildMemberAdd", async (user) => {
-    const userId = user.id;
+    let id = user.id;
 
-    userList[userId] = setTimeout(async function () {
-        try {
-            const member = await user.guild.members.fetch(userId);
-
-            if (member && !member.roles.cache.has(REQUIRED_ROLE)) {
-                // user failed to complete onboarding in time
-                delete userList[userId];
-                await member.kick("Failed to complete onboarding in time");
-            }
-        } catch (err) {
-            console.log(`Error for ${userId}: ${err.message}`);
-        }
-
-        if (userList[userId]) delete userList[userId];
-    }, ROLE_CHECK_MS);
-});
-
-client.on("guildMemberRemove", async (user) => {
-    const userId = user.id;
     try {
-        if (userList[userId]) {
-            clearTimeout(userList[userId]);
-            delete userList[userId];
+        const accountAge = user.user.createdAt.valueOf();
+        const AGES_AGO = new Date(new Date().getTime() - (MONTHS_LIMIT * 2)).valueOf();
+        const MONTHS_AGO = new Date(new Date().getTime() - MONTHS_LIMIT).valueOf();
+        // check if they rejoined
+        if (!ids[id] && accountAge > AGES_AGO) {
+            // kick and record
+            ids[id] = {
+                date: new Date().valueOf(),
+                // users older accounts can be trusted more
+                count: accountAge > MONTHS_AGO ? 0 : JOIN_COUNT
+            };
+            await user.kick(`Raid account: let in after ${JOIN_COUNT - ids[id].count}`);
+            console.log(`First Kick: ${id}`);
+        } else if (ids[id].count < JOIN_COUNT) {
+            // kick and record
+            ids[id].count++;
+            ids[id].date = new Date().valueOf();
+            await user.kick("raid account");
+            console.log(`Raid account: let in after ${JOIN_COUNT - ids[id].count}`);
+        } else {
+            // let them stay, no need to track them
+            delete ids[id];
+            console.log(`Allow: ${id}`);
         }
     } catch (err) {
         console.log(`${id}: ${err.message}`);
     }
 });
+
+// clear out stale IDs after 12 hours
+setInterval(function() {
+    try {
+        const HOURS_AGO = new Date(new Date().getTime() - HOURS_LIMIT).valueOf();
+        Object.keys(ids).forEach(id => {
+            if (ids[id].date < HOURS_AGO) {
+                delete ids[id];
+                console.log(`Stale: ${id}`);
+            }
+        });
+    } catch (err) {
+        console.log(`Unable to clear stale objects: ${err.message}`);
+    }
+}, HOURS_LIMIT);
 
 client.login(process.env.TOKEN);
