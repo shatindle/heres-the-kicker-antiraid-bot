142fa9f83398285ae0113f2ebd95bdfd1daedc62
Shane Tindle <sha.tindle@gmail.com>
2023-05-22 08:24:23 -0500

    Added channel logging to the bot

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ let ids = {};
 const JOIN_COUNT = parseInt(process.env.JOIN_COUNT);
 const HOURS_LIMIT = 1000 * 60 * 60 * 12;
 const MONTHS_LIMIT = 1000 * 60 * 60 * 24 * 30 * 8;
+const LOG_TO = process.env.LOG_TO;
 
 client.on("guildMemberAdd", async (user) => {
     let id = user.id;
-    let guildId = user.guild.id;
+    let guild = user.guild;
+    let guildId = guild.id;
 
     try {
         const accountAge = user.user.createdAt.valueOf();
@@ -36,17 +38,17 @@ client.on("guildMemberAdd", async (user) => {
                 count: accountAge > MONTHS_AGO ? 0 : JOIN_COUNT
             };
             await user.kick(`Raid account: let in after ${JOIN_COUNT - ids[guildId][id].count}`);
-            console.log(`First Kick: ${id}`);
+            await client.channels.cache.get(LOG_TO).send(`Remaining kicks ${accountAge > MONTHS_AGO ? 0 : JOIN_COUNT}: ${id}`);
         } else if (ids[guildId][id] && ids[guildId][id].count < JOIN_COUNT) {
             // kick and record
             ids[guildId][id].count++;
             ids[guildId][id].date = new Date().valueOf();
             await user.kick("raid account");
-            console.log(`Raid account: let in after ${JOIN_COUNT - ids[guildId][id].count}`);
+            await client.channels.cache.get(LOG_TO).send(`Remaining kicks ${JOIN_COUNT - ids[guildId][id].count}: ${id}`);
         } else if (ids[guildId][id]) {
             // let them stay, no need to track them
             delete ids[guildId][id];
-            console.log(`Allow: ${id}`);
+            await client.channels.cache.get(LOG_TO).send(`Allow: ${id}`);
         }
     } catch (err) {
         console.log(`${id}: ${err.message}`);
